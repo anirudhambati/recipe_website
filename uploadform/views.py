2b504e90f6dea2808f97a409bcf2cff60e2b2f1d
Anirudh Ambati <anirudhambati@gmail.com>
2019-04-07 15:47:56 +0530

    Update views.py

--- uploadform/views.py
+++ uploadform/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-import cv2
+#import cv2
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
 from django.shortcuts import render
